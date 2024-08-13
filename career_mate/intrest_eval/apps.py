d1d8aeb848a5c172e286cb34898db69a5dda5774
Pras2005 <146535227+Pras2005@users.noreply.github.com>
2024-08-13 19:47:57 +0530

    first commit

---
+++ career_mate/intrest_eval/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IntrestEvalConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "intrest_eval"
---
+++ career_mate/skill_eval/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SkillEvalConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "skill_eval"
